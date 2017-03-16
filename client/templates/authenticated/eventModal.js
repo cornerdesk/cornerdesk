34ecc68edf3df76b5672879f826a8b3bf7ec70bd
Béranger Campardou <b.campardou@gmail.com>
2017-03-16 10:53:28 +0100

    Calendrier actif dans la sidebar

--- client/templates/authenticated/eventModal.js
+++ client/templates/authenticated/eventModal.js
@@ -44,32 +44,34 @@ Template.eventModal.events({
 });
 
 Template.eventModal.onRendered(() => {
-    let $form = jQuery('#eventModal form');
-    let event = Session.get('selectedEvent');
-    $form.find('#event-title').val(event.title);
-    $form.find('#eventColorPicker').colorpicker({
-        color: event.color,
-        colorSelectors: {
-            'black': '#000000',
-            'white': '#ffffff',
-            'red': '#FF0000',
-            'default': '#777777',
-            'primary': '#337ab7',
-            'success': '#5cb85c',
-            'info': '#5bc0de',
-            'warning': '#f0ad4e',
-            'danger': '#d9534f'
-        },
-        horizontal: true
-    });
+    Tracker.autorun(() => {
+        let $form = jQuery('#eventModal form');
+        let event = Session.get('selectedEvent');
+        $form.find('#event-title').val(event.title);
+        $form.find('#eventColorPicker').colorpicker({
+            color: event.color,
+            colorSelectors: {
+                'black': '#000000',
+                'white': '#ffffff',
+                'red': '#FF0000',
+                'default': '#777777',
+                'primary': '#337ab7',
+                'success': '#5cb85c',
+                'info': '#5bc0de',
+                'warning': '#f0ad4e',
+                'danger': '#d9534f'
+            },
+            horizontal: true
+        });
 
-    $form.find('#event-private').attr('checked', event.isPrivate);
-    $form.find('#event-allday').attr('checked', event.allDay);
-    $form.find('#event-start').datetimepicker({ format: dateFormat, defaultDate: moment(event.start.toISOString()).format() });
-    $form.find('#event-end').datetimepicker({ format: dateFormat, defaultDate: moment(event.end.toISOString()).format() });
-    $form.find('#event-id').val(event._id);
-    $form.find('#event-description').val(event.description);
-    $("#event-start").on("dp.change", (e) => {
-        $('#event-end').data("DateTimePicker").minDate(e.date);
+        $form.find('#event-private').attr('checked', event.isPrivate);
+        $form.find('#event-allday').attr('checked', event.allDay);
+        $form.find('#event-start').datetimepicker({ format: dateFormat, defaultDate: moment(event.start.toISOString()).format() }).data('DateTimePicker');
+        $form.find('#event-end').datetimepicker({ format: dateFormat, defaultDate: moment(event.end.toISOString()).format() });
+        $form.find('#event-id').val(event._id);
+        $form.find('#event-description').val(event.description);
+        $("#event-start").on("dp.change", (e) => {
+            $('#event-end').data("DateTimePicker").minDate(e.date);
+        });
     });
 });
--- client/templates/authenticated/sidebar.js
+++ client/templates/authenticated/sidebar.js
@@ -1,40 +1,52 @@
-Template.sidebar.onCreated( () => {
+Template.sidebar.onCreated(() => {
   let template = Template.instance();
-  template.subscribe( 'sidebar' );
+  template.subscribe('sidebar');
 });
 
 Template.sidebar.helpers({
-  currentChannel( name ) {
-    let current = FlowRouter.getParam( 'channel' );
-    if ( current ) {
-      return current === name || current === `@${ name }` ? 'active' : false;
+  currentChannel(name) {
+    if (FlowRouter.getRouteName() === 'channel') {
+      let current = FlowRouter.getParam('channel');
+      if (current) {
+        return current === name || current === `@${name}` ? 'active' : false;
+      }
     }
+    return false;
+  },
+  currentCalendar(name) {
+    if (FlowRouter.getRouteName() === 'calendar') {
+      let current = FlowRouter.getParam('calendar');
+      if (current) {
+        return current === name || current === `@${name}` ? 'active' : false;
+      }
+    }
+    return false;
   },
   currentUsername() {
-    let currentUser = Meteor.users.findOne( { _id: Meteor.userId() } );
+    let currentUser = Meteor.users.findOne({ _id: Meteor.userId() });
     return currentUser.username;
   },
   calendars() {
     let calendars = Calendars.find();
-    if ( calendars ) {
+    if (calendars) {
       return calendars;
     }
   },
   channels() {
     let channels = Channels.find();
-    if ( channels ) {
+    if (channels) {
       return channels;
     }
   },
   users() {
-    let users = Meteor.users.find( { _id: { $ne: Meteor.userId() } } );
-    if ( users ) {
+    let users = Meteor.users.find({ _id: { $ne: Meteor.userId() } });
+    if (users) {
       return users;
     }
   },
-  fullName( name ) {
-    if ( name ) {
-      return `${ name.first } ${ name.last }`;
+  fullName(name) {
+    if (name) {
+      return `${name.first} ${name.last}`;
     }
   }
 });
