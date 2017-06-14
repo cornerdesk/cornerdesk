bac93169393f2a9a826afb5ad261633667abcb68
BaobabCoder <b.campardou@gmail.com>
2017-06-14 14:02:39 +0200

    remove vorlon

--- server/startup.js
+++ server/startup.js
@@ -4,7 +4,4 @@ import seedDatabase from './modules/seed-database';
 Meteor.startup(() => {
     setBrowserPolicies();
     seedDatabase();
-    BrowserPolicy.content.allowOriginForAll('*.ngrok.io');
-    BrowserPolicy.content.allowOriginForAll('blob:');
-    BrowserPolicy.content.allowDataUrlForAll();
 });
